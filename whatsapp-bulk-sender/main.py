da5cd4782d95c9b8ae08fc43a7adb62707112ff4
JonnyMarcus <jonnymarcusmilitao123@gmail.com>
2026-06-19 16:37:46 -0300

    feat: adicionando loop para mandar mensagem para os numeros

---
+++ whatsapp-bulk-sender/main.py
@@ -0,0 +1,13 @@
+from supabase_client import buscar_contatos
+from zapi_client import enviar_mensagem
+
+
+if __name__ == "__main__":
+    contatos = buscar_contatos()
+
+    for contato in contatos:
+        nome = contato["Nome"]
+        telefone = contato["Telefone"]
+        resultado = enviar_mensagem(telefone, nome)
+        print(f"Enviado para {nome}: {resultado}")
+
